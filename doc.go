78cb22e873c636e2844e2cb0a27caa078e2f11bc
Deluan <deluan@deluan.com>
2019-10-02 17:16:04 -0400

    Add maintainability badge

--- doc.go
+++ doc.go
@@ -1,3 +1,4 @@
-// Bring implements a Guacamole client, with full remote control capabilities.
-//
+/*
+	Package bring implements a Guacamole client, with full remote control capabilities.
+*/
 package bring
