4a3a3b66e4db82719848ce826e65778bc32e2820
Deluan <deluan@deluan.com>
2019-11-22 13:52:26 -0500

    Fixing time-dependent session test in Windows

--- session_test.go
+++ session_test.go
@@ -1,28 +1,38 @@
 package bring
 
 import (
+	"time"
+
 	"github.com/deluan/bring/protocol"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Session", func() {
-	server := &fakeServer{
-		replies: map[string]string{
-			"select":  "4.args,8.hostname,4.port,8.password;",
-			"connect": "5.ready,21.$unique-connection-id;",
-		},
-	}
-	addr := server.start()
-	s, _ := newSession(addr, "rdp", map[string]string{
-		"hostname": "host1",
-		"port":     "port1",
-		"password": "password123",
-	}, &DefaultLogger{Quiet: true})
+	var (
+		server *fakeServer
+		s      *session
+	)
+	BeforeEach(func() {
+		server = &fakeServer{
+			replies: map[string]string{
+				"select":  "4.args,8.hostname,4.port,8.password;",
+				"connect": "5.ready,21.$unique-connection-id;",
+			},
+		}
+		addr := server.start()
+		s, _ = newSession(addr, "rdp", map[string]string{
+			"hostname": "host1",
+			"port":     "port1",
+			"password": "password123",
+		}, &DefaultLogger{Quiet: true})
+
+		Eventually(func() SessionState {
+			return s.State
+		}, 3*time.Second, 100*time.Millisecond).Should(Equal(SessionActive))
+	})
 
 	It("should handshake with server", func() {
-		Eventually(s.State).Should(Equal(SessionActive))
-
 		err := s.Send(protocol.NewInstruction(disconnectOpcode))
 		Expect(err).To(BeNil())
 
